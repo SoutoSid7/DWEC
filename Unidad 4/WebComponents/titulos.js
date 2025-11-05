0cf0c5656d83b8135f68185d1d3e4f4e551d3ea4
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-11-05 13:19:41 +0100

    05/11/2025

---
+++ Unidad 4/WebComponents/titulos.js
@@ -0,0 +1,14 @@
+const titulos = [
+    {
+        titulo: "Las cinco semillas de naranja",
+        subtitulo: "La entrega de una carta inofensiva seguida de la muerte.",
+    },
+    {
+        titulo: "Estudio en escarlata",
+        subtitulo: "El Dr. Watson conoce a Sherlock Holmes",
+    },
+    {
+        titulo: "El perro de Baskerville",
+        subtitulo: "Un perro misterioso aterroriza a una ciudad.",
+    },
+];
