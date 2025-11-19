e06b1c4e90763b976386e376b16dee045dfdef47
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-11-19 13:26:28 +0100

    React

--- Unidad 5/hola-mundo/src/App.jsx
+++ Unidad 5/hola-mundo/src/App.jsx
@@ -4,9 +4,10 @@ import viteLogo from '/vite.svg'
 import AcercaDe from './components/AcercaDe'
 import Saludar from './components/Saludar'
 import './App.css'
+import { userState } from "react";
 
 function App() {
-  const usuario = {
+  /* const usuario = {
     nombre: "Mario Souto",
     edad: 19,
     color: "Rojo"
@@ -15,6 +16,15 @@ function App() {
     <>
       <Saludar userinfo={usuario} />
     </>
-  ) 
+  ) */
+  const [contador, setContador] = useState(0)
+  return(
+    <>
+      <h1>Estado simple: {contador}</h1>
+      <button onClick={() => setContador(contador+1)}>
+        Cambio de estado
+      </button>
+    </>
+  )
 }
 export default App;
